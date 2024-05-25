9e0e2bfccf08713adc3aaa93d66c9d9c8ea1cf6b
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-05-26 04:13:15 +0600

    chunks created

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
+import { splitJsonFileIntoChunks } from "./organizeJson/divideJson";
 import { setCategory } from "./setCategory/setCategory";
 import { updateLibraryData } from "./updateLibraryData/updateLibraryData";
 
 // setCategory().catch(console.error);
-updateLibraryData().catch(console.error);
+// updateLibraryData().catch(console.error);
+splitJsonFileIntoChunks()
