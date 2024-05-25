86edeb37884c5d756e2489ba06086ffaa21616ca
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-05-25 07:30:04 +0600

    writing on json

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // Get the directory name in an ES module context
-// @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -90,156 +89,34 @@ async function main() {
   const filePath = path.join(__dirname, "libraries.json");
   const rawItems: raw_items_types = JSON.parse(readFileSync(filePath, "utf8"));
 
-  const categories = new Set<string>();
+  for (const library of rawItems.libraries) {
+    console.log(
+      `Processing library: ${
+        library.github?.fullName || library.npmPkg || "Unknown"
+      }`
+    );
 
-  rawItems.libraries.forEach((library) => {
-    console.log("npmPkg:", library.npmPkg);
     if (library.topicSearchString) {
       const topicCategories = library.topicSearchString.split(" ");
-      topicCategories.forEach((category) => categories.add(category));
+      const selectedCategory = await select({
+        message: `Select a category to add for ${
+          library.github?.fullName || library.npmPkg || "Unknown"
+        }:`,
+        options: topicCategories.map((category) => ({
+          name: category,
+          value: category,
+        })),
+      });
+
+      library.category = library.category || [];
+      library.category.push(...selectedCategory);
+
+      writeFileSync(filePath, JSON.stringify(rawItems, null, 2));
+      console.log("Updated library:", library);
+    } else {
+      console.log("No topicSearchString available for this library.");
     }
-  });
-
-  const categoryArray = Array.from(categories);
-  const options = [
-    ...rawItems.libraries.map((library) => ({
-      name: library.github?.fullName || library.npmPkg || "Unknown",
-      value: library,
-    })),
-    ...categoryArray.map((category) => ({
-      name: category,
-      value: category,
-    })),
-  ];
-
-  // Use inquirer-select-pro to select an option
-  const selected = await select({
-    message: "Select a library or a category:",
-    options,
-  });
-
-  if (typeof selected !== "string") {
-    const selectedLibrary = selected as Library;
-    const selectedCategory = await select({
-      message: "Select a category to add:",
-      options: categoryArray.map((category) => ({
-        name: category,
-        value: category,
-      })),
-    });
-
-    selectedLibrary.category = selectedLibrary.category || [];
-    selectedLibrary.category.push(selectedCategory);
-
-    writeFileSync(filePath, JSON.stringify(rawItems, null, 2));
-    console.log("Updated library:", selectedLibrary);
   }
 }
 
 main().catch(console.error);
-
-// import { argv } from "node:process";
-// import { writeFile, readFileSync, existsSync } from "fs";
-// import {
-//   filterLocalData,
-//   filterRemoteData,
-//   remoteData,
-//   top100Films,
-// } from "./sampleData";
-// import { SelectProps, select } from "inquirer-select-pro";
-// import path from "path";
-// import { fileURLToPath } from "url";
-
-// // Get the directory name in an ES module context
-// // @ts-ignore
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// function createDemo<Value, Multiple extends boolean = true>(
-//   baseProps: SelectProps<Value, Multiple>
-// ) {
-//   return async (
-//     props: Partial<SelectProps<Value, Multiple>>,
-//     demoName: string
-//   ) => {
-//     const answer = await select<Value, Multiple>({
-//       ...baseProps,
-//       ...props,
-//     });
-//     console.log(answer);
-
-//     // Define the file path
-//     const filePath = path.join(__dirname, `${demoName}.json`);
-
-//     // Write the selected data directly to the file
-//     writeFile(filePath, JSON.stringify(answer, null, 2), (err) => {
-//       if (err) {
-//         console.error("Error writing to file:", err);
-//       } else {
-//         console.log(`Data saved to ${demoName}.json`);
-//       }
-//     });
-//   };
-// }
-
-// const options = {
-//   local: createDemo({
-//     message: "Select all the movies you want to watch:",
-//     options: top100Films,
-//   }),
-//   remote: createDemo({
-//     message: "Select all the movies you want to watch:",
-//     options: remoteData,
-//   }),
-//   "filter-remote": createDemo({
-//     message: "Select all the movies you want to watch:",
-//     options: filterRemoteData,
-//   }),
-//   "filter-local": createDemo({
-//     message: "Select all the movies you want to watch:",
-//     options: filterLocalData,
-//   }),
-// };
-
-// type Demos = keyof typeof options;
-
-// const availableOptions = [
-//   "filter",
-//   "clearInputWhenSelected",
-//   "required",
-//   "loop",
-//   "multiple",
-//   "canToggleAll",
-// ];
-
-// let whichDemo: Demos | null;
-
-// const demos = Object.keys(options) as Demos[];
-// const flags: any = {};
-// for (let index = argv.length - 1; index >= 0; index--) {
-//   const arg = argv[index];
-//   let g = null;
-//   if (demos.indexOf(arg as Demos) >= 0) {
-//     whichDemo = arg as Demos;
-//     break;
-//   } else if ((g = arg.match(/--(\w+)=?(true|false)?/))) {
-//     if (availableOptions.includes(g[1])) {
-//       flags[g[1]] = g[2] === undefined || g[2] === "true";
-//     }
-//   }
-// }
-
-// // @ts-ignore
-// if (!whichDemo) {
-//   // @ts-ignore
-//   whichDemo = await select({
-//     message: "Which demo do you want to run?",
-//     multiple: false,
-//     options: demos.map((value) => ({
-//       name: value,
-//       value,
-//     })),
-//   });
-// }
-// // @ts-ignore
-// options?.[whichDemo as keyof typeof options]?.(flags, whichDemo);
