2eecf8e38f10900034d70db3bb027ea83066ac7f
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-05-27 08:48:46 +0600

    organize files

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+import { categorizeLibraries } from "./organizeJson/categorizeLibraries";
 import { splitJsonFileIntoChunks } from "./organizeJson/divideJson";
 import { removeDuplicates } from "./organizeJson/removeDuplicate";
+import { removeDuplicatesAndMergeCategories } from "./organizeJson/removeDuplicatesAndMergeCategories";
 import { ensureUniqueItems } from "./organizeJson/removeDuplicatesWithin";
 import { processLibraries } from "./setCategory/processLibraries";
 import { setCategory } from "./setCategory/setCategory";
 import { updateLibraryData } from "./updateLibraryData/updateLibraryData";
+categorizeLibraries().catch(console.error);
+// removeDuplicatesAndMergeCategories().catch(console.error);
 // removeDuplicates().catch(console.error);
 // ensureUniqueItems();
-setCategory().catch(console.error);
+// setCategory().catch(console.error);
 // processLibraries().catch(console.error);
 // updateLibraryData().catch(console.error);
 // splitJsonFileIntoChunks()
